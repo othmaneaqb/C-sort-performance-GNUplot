8b47b51eaf04cf4767741ca2d0bfe5b4eca2fdce
othmaneaqb <162018051+othmaneaqb@users.noreply.github.com>
2025-12-10 01:56:20 +0100

    Add sorting algorithms and swap helper function

--- include/sorts.h
+++ include/sorts.h
@@ -8,4 +8,12 @@ void quick_sort(int *arr, int n);
 void merge_sort(int *arr, int n);
 void heap_sort(int *arr, int n);
 
+// Small helper: swap two numbers
+void swap(int *a, int *b);
+
+// The three sorting algorithms we implemented
+void insertionSort(int arr[], int n);
+void selectionSort(int arr[], int n);
+void shellSort(int arr[], int n);
+
 #endif
